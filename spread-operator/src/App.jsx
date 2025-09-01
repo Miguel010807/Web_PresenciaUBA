aecf198cf76de29a76c2b24844dca0e13dd0bbfb
Miguel010807 <mdiaz@etec.uba.ar>
2025-09-01 15:34:16 -0300

    Agregamos mas funciones en los componentes

--- spread-operator/src/App.jsx
+++ spread-operator/src/App.jsx
@@ -2,24 +2,20 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { Operator } from "./components/operator";
 
-
 function App() {
- const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    //useEffect ejecuta el codigo una vez si no hay nada en los
+    //corchetes, y si hay variables(dependencias)
+    //se va a ejecutar tanto como actualice
+  }, []);
 
- useEffect(() => {
-   //useEffect ejecuta el codigo una vez si no hay nada en los
-   //corchetes, y si hay variables(dependencias)
-   //se va a ejecutar tanto como actualice
- }, []);
-
-
- return (
-   <>
-     <Operator />
-   </>
- );
+  return (
+    <>
+      <Operator />
+    </>
+  );
 }
-
 
 export default App;
--- spread-operator/src/components/operator.jsx
+++ spread-operator/src/components/operator.jsx
@@ -1,48 +1,64 @@
 import { useState } from "react";
 
-
 export function Operator() {
- const [nums, setNums] = useState([1, 2, 3, 4, 5]);
- const [persona, setPersona] = useState({
-   name: "Federico",
-   alumno: "Thiago",
- });
+  const [nums, setNums] = useState([1, 2, 3, 4, 5]);
+  const [persona, setPersona] = useState({
+    name: "Federico",
+    alumno: "Thiago",
+  });
+  const [productos, setProductos] = useState([
+    {
+      nombre: "Camiseta",
+      precio: 20,
+    },
 
+    { nombre: "Pantalon", precio: 30 },
+  ]);
 
- //arrow function
- const addNums = () => {
-   const ultimo = nums [nums.length - 1] //<---- Este const me dice el ultimo numero del Array
-   setNums([...nums, ultimo + 1 ]);
- };
- // no use "setNums ([...nums.length + 1]);" porque en el caso que sean numeros mas grandes
- // no iva afuncionar, osea si, pero no
- // por que la el "nums.length" te dice la cantidad de elementos adentros
- // del Array, y no el valor de los elementos
+  //arrow function
+  const addNums = () => {
+    const ultimo = nums[nums.length - 1]; //<---- Este const me dice el ultimo numero del Array
+    setNums([...nums, ultimo + 1]);
+  };
+  // no use "setNums ([...nums.length + 1]);" porque en el caso que sean numeros mas grandes
+  // no iva afuncionar, osea si, pero no
+  // por que la el "nums.length" te dice la cantidad de elementos adentros
+  // del Array, y no el valor de los elementos
 
- const updateName = () => {
-   setPersona({ ...persona, name: "Miguel", edad: 80 });
-   console.log(persona);
- };
+  const updateName = () => {
+    setPersona({ ...persona, name: "Miguel", edad: 80 });
+    console.log(persona);
+  };
 
+  const updateDescuentoCorrecto = () => {
+    const newArr = productos.map((item) => ({
+      ...item,
+      descuento: true,
+    }));
+    console.log(newArr);
+    setProductos(newArr);
+  };
 
- return (
-   <>
-     {nums.map((num, index) => (
-       <p key={index}>{num}</p>
-     ))}
-     <h3>Spread Operator</h3>
-     <button onClick={addNums}>agregar numeros</button>
-     <button onClick={updateName}>modificar persona</button>
-   </>
- );
-}
-
-
-const Productos=[{ nombre: 'Camiseta', precio: 20},
-                 {nombre: 'Pantalon', precio: 30 }]
-
-
-const ProdcutosConDescuento = Productos.map(produ =>({
+  /*
   
-  ...produ, descuento: "True" })) 
+  const updateDescuento = () => {
+    const newArr = productos.map((item) => {
+      setProductos([{ ...item, descuento: true }]);
+    });
+    console.log(newArr);
+  };
+  
+  */
 
+  return (
+    <>
+      {nums.map((num, index) => (
+        <p key={index}>{num}</p>
+      ))}
+      <h3>Spread Operator</h3>
+      <button onClick={addNums}>agregar numeros</button>
+      <button onClick={updateName}>modificar persona</button>
+      <button onClick={updateDescuentoCorrecto}> ponerDescuento </button>
+    </>
+  );
+}
