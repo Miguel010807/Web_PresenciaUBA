bf82bf9fd7615e947d46609acd6e565630c73a1b
Miguel010807 <mdiaz@etec.uba.ar>
2025-09-02 16:18:11 -0300

    Agrege una tabla mas

--- presenciauba/sql/usuario1.sql
+++ presenciauba/sql/usuario1.sql
@@ -42,5 +42,17 @@ CREATE TABLE "presenciaGeneral" (
 	FOREIGN KEY("id_usuario") REFERENCES "usuarios"("id_usuario")
 );
 
+CREATE TABLE "asistenciaMateria" (
+	"id_asistencia_materia"	INTEGER NOT NULL,
+	"id_usuario"	INTEGER NOT NULL,
+	"id_materia"	INTEGER NOT NULL,
+	"fecha"	TEXT NOT NULL,
+	"hora_registro"	TEXT NOT NULL,
+	"qr_validado"	INTEGER NOT NULL CHECK(qr_svalidado IN (0,1)),
+	FOREIGN KEY("id_usuario") REFERENCES "usuarios"("id_usuario"),
+	FOREIGN KEY("id_materia") REFERENCES "materia"("id_materia"),
+	PRIMARY KEY("id_asistencia_materia" AUTOINCREMENT)
+);
+
 
 
